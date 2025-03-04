308b78ca46e2385ad6c02a30c544e47bd0c39fe4
GBarbosa <andradegui2006@gmail.com>
2025-03-04 20:25:57 -0300

    componente task finalizado

--- react-app/src/App.jsx
+++ react-app/src/App.jsx
@@ -32,13 +32,19 @@ function App(){
       )
     )
   }
+  function onTrashClick(taskId) {
+    setTasks(taskList => taskList.filter(task => task.id !== taskId));
+  }
 
   return(
     <div className="w-screen h-screen bg-slate-500 flex justify-center p-6">
       <div className="w-[500px] ">
         <h1 className="text-3xl text-slate-100 font-bold text-center">Gerenciador de Tarefas</h1>
-      <Tasks tasks={tasks} onTaskClick={onTaskClick}/>
+
       <AddTask/>
+
+      <Tasks tasks={tasks} onTaskClick={onTaskClick} onTrashClick={onTrashClick} />
+      
       </div>
     </div>
   );
--- react-app/src/components/Tasks.jsx
+++ react-app/src/components/Tasks.jsx
@@ -1,18 +1,18 @@
-import {ChevronRightIcon} from "lucide-react"
-function Tasks(props) {
+import {ChevronRightIcon, TrashIcon} from "lucide-react"
+function Tasks({tasks, onTaskClick, onTrashClick}) {
     return(
         <ul className="space-y-4 p-6 bg-slate-200 rounded-md shadow">
             {
-                props.tasks.map((task) => (
+                tasks.map((task) => (
                     <li key={task.id} className="flex gap-2">
-                        <button onClick={() => props.onTaskClick(task.id)} className={`w-full text-left bg-slate-400 text-white p-2 rounded-md ${task.isCompleted && "line-through"}`}>
+                        <button onClick={() => onTaskClick(task.id)} className={`w-full text-left bg-slate-400 text-white p-2 rounded-md ${task.isCompleted && "line-through"}`}>
                             {task.title}
                         </button>
                         <button className="bg-slate-400 p-2 rounded-md text-white">
                             <ChevronRightIcon />
                         </button>
-                        <button className="bg-slate-400 p-2 rounded-md text-white">
-                            <TrashIcon /> //implementar função de deletar tarefas
+                        <button onClick={() => onTrashClick(task.id)} className="bg-slate-400 p-2 rounded-md text-white">
+                            <TrashIcon /> 
                         </button>
                     </li>
                 ))
